ad14f149a4ee790537244b323b07470d25e69d3e
ALQADHI-HK <111786165+ALQADHI-HK@users.noreply.github.com>
2022-09-22 15:15:43 +0300

    Add files via upload

---
+++ ddos.sh
@@ -0,0 +1,17 @@
+#copyright : @an_danh
+# DDoS Tools Copright : an_danh
+#github : https://github.com/hacker2008274
+#telegram : 0978842516:@an_danh
+#we are no_team
+
+clear
+figlet ALQADHI_HK DDoS 
+echo -e "\e[3;84m[:::] ...::: DDoS Tools Copright : ALQADHI_HK                 :::... [:::]"
+echo -e "\e[2;52m[:::] ...::: github : https://github.com/hacker2008274        :::... [:::]"
+echo -e "\e[3;48m[:::] ...::: telegram : https://t.me/ALQADHI_HK               :::... [:::]" 
+echo -e "\033[3;92m[:::]...::: Instagram :    [ g_yqt  ]                       :::... [:::]"
+echo -e "\e[1;34m[:::] Target (example inter the URL here : https://url.com/)  : "
+read target
+echo -e "\033[3;93m[:::]         I am ALQADHI_HK YEMEN         [:::]"
+sleep 5
+go run 1.go -site $target
