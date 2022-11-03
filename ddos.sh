56e5b07e489283492ee16da13d2ba1d3948fab4c
ALQADHI_HK <111786165+ALQADHI-HK@users.noreply.github.com>
2022-11-03 14:21:54 +0300

    Update ddos.sh

--- ddos.sh
+++ ddos.sh
@@ -7,7 +7,7 @@
 clear
 figlet ALQADHI DDoS 
 echo -e "\e[3;84m[:::] ...::: DDoS Tools Copright : ALQADHI_HK                 :::... [:::]"
-echo -e "\e[2;52m[:::] ...::: github : https://github.com/hacker2008274        :::... [:::]"
+echo -e "\e[2;52m[:::] ...::: github : https://github.com/ALQADHI_HK           :::... [:::]"
 echo -e "\e[3;48m[:::] ...::: telegram : https://t.me/ALQADHI_HK               :::... [:::]" 
 echo -e "\033[3;92m[:::]...::: Instagram :    [ g_yqt  ]                       :::... [:::]"
 echo -e "\e[1;34m[:::] Target (example inter the URL here : https://url.com/)  : "
